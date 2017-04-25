2cad84c61c2a12d66c13a24c7605ebd1ede8993c
dzgermen <dogazgermen@gmail.com>
2017-04-25 18:14:27 +0300

    Manager Classes

---
+++ SoldierProductionManager.java
@@ -0,0 +1,18 @@
+
+public class SoldierProductionManager extends Manager{
+	
+	public  SoldierProductionManager(Game game)
+	{
+		super(game);
+	}
+	public ArmyUnit[] produceArmyUnitType(int type,int amount)
+	{
+		ArmyUnit [] armyunit = new ArmyUnit (amount);
+		
+		for (int i = 0 ; i < amount ; i++)
+		{
+			//will be filled
+		}
+	}
+
+}
