164734bad871a5b5ebe4e30ed102b2d33def7654
dzgermen <dogazgermen@gmail.com>
2017-04-25 18:13:39 +0300

    Manager Classes

---
+++ BattleManager.java
@@ -0,0 +1,20 @@
+
+public class BattleManager extends Manager{
+	
+	public BattleManager (Game game)
+	{
+		super(game);
+	}
+	BattleSystem battle = new Battle();
+	
+	Player player = new Player ();
+	
+	public void executeBattle()
+	{
+		
+	}
+	public Player getWinner()
+	{
+		
+	}
+}
