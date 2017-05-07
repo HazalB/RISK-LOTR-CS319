c2e7bb8eaaf40602bbf6725c662d8a94dfeb2a0a
Hazal Buruk <asli_deniz_hazal@hotmail.com>
2017-05-08 00:27:38 +0300

    Add files via upload

---
+++ AddPlayerPanel.java
@@ -0,0 +1,266 @@
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class AddPlayerPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	private JPanel mainPanel;
+	private CardLayout cardLayout;
+	private JButton nextBut;
+	private Image background;
+	private JTextField askName;
+	private String name;
+	private JRadioButton rohan, gondor, elves, dwarfs, mordor, harad, isengard;
+	private int totalPlayerNum, currentFaction;
+	private ButtonGroup bG;
+	private ArrayList<String> nameList, factionList;
+	private JLabel label;
+	
+	
+	public AddPlayerPanel(JPanel mP, CardLayout cl) {
+		
+		currentFaction=0;
+		bG=new ButtonGroup();
+		
+		nameList = new ArrayList<String>();
+		factionList = new ArrayList<String>();		
+		
+		name = "";
+		
+		label = new JLabel();
+		label.setText(Integer.toString(totalPlayerNum));
+		label.setBounds(400, 300, 100, 100);
+		add(label);
+		
+		mainPanel = mP;
+		cardLayout = cl;
+		
+		nextBut = new JButton();
+		
+		askName = new JTextField();
+		askName.addActionListener(new NameListener());
+		
+		rohan = new JRadioButton("Rohan");
+		gondor = new JRadioButton("Gondor");
+		elves = new JRadioButton("Elves");
+		dwarfs = new JRadioButton("Dwarfs");
+		mordor = new JRadioButton("Mordor");
+		harad = new JRadioButton("Harad");
+		isengard = new JRadioButton("Isengard");
+		
+		add(rohan);
+		add(gondor);
+		add(elves);
+		add(dwarfs);
+		add(mordor);
+		add(harad);
+		add(isengard);
+		
+		bG.add(rohan);
+		bG.add(gondor);
+		bG.add(elves);
+		bG.add(dwarfs);
+		bG.add(mordor);
+		bG.add(harad);
+		bG.add(isengard);
+		
+		rohan.setBounds(218, 275, 67, 20);
+		rohan.setFocusPainted(false); 
+		rohan.setOpaque(false);
+		rohan.setContentAreaFilled(false);
+		rohan.setBorderPainted(false);	
+		rohan.setFocusable(false);
+		
+		gondor.setBounds(392, 275, 67, 20);
+		gondor.setFocusPainted(false); 
+		gondor.setOpaque(false);
+		gondor.setContentAreaFilled(false);
+		gondor.setBorderPainted(false);	
+		gondor.setFocusable(false);
+		
+		elves.setBounds(275, 275, 67, 20);
+		elves.setFocusPainted(false); 
+		elves.setOpaque(false);
+		elves.setContentAreaFilled(false);
+		elves.setBorderPainted(false);	
+		elves.setFocusable(false);
+		
+		dwarfs.setBounds(330, 275, 67, 20);
+		dwarfs.setFocusPainted(false); 
+		dwarfs.setOpaque(false);
+		dwarfs.setContentAreaFilled(false);
+		dwarfs.setBorderPainted(false);	
+		dwarfs.setFocusable(false);
+		
+		mordor.setBounds(218, 295, 67, 20);
+		mordor.setFocusPainted(false); 
+		mordor.setOpaque(false);
+		mordor.setContentAreaFilled(false);
+		mordor.setBorderPainted(false);	
+		mordor.setFocusable(false);
+		
+		harad.setBounds(278, 295, 67, 20);
+		harad.setFocusPainted(false); 
+		harad.setOpaque(false);
+		harad.setContentAreaFilled(false);
+		harad.setBorderPainted(false);	
+		harad.setFocusable(false);
+		
+		isengard.setBounds(333, 295, 75, 20);
+		isengard.setFocusPainted(false); 
+		isengard.setOpaque(false);
+		isengard.setContentAreaFilled(false);
+		isengard.setBorderPainted(false);	
+		isengard.setFocusable(false);
+		
+		rohan.addActionListener(new FactionListener());
+		mordor.addActionListener(new FactionListener());
+		harad.addActionListener(new FactionListener());
+		isengard.addActionListener(new FactionListener());
+		dwarfs.addActionListener(new FactionListener());
+		elves.addActionListener(new FactionListener());
+		gondor.addActionListener(new FactionListener());
+		
+		nextBut.setFocusPainted(false); 
+		nextBut.setOpaque(false);
+		nextBut.setContentAreaFilled(false);
+		nextBut.setBorderPainted(false);	
+		nextBut.setFocusable(false);
+		
+		nextBut.setIcon(new ImageIcon("images/next.png"));
+		
+		nextBut.addActionListener(new ButtonListener());
+		
+		addKeyListener(new BackToMenuListener());
+		add(nextBut);
+		add(askName);
+		
+		nextBut.setBounds(228, 335, 95, 38);
+		askName.setBounds(241, 200, 100, 20);
+		
+		setLayout(null);
+		setBackground(Color.black);
+		setVisible(true);
+		setBounds(0, 0, 650, 650);
+		
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);		
+		background = new ImageIcon("images/askforplayer.png").getImage(); //get background icon as image
+		
+		g.drawImage( background, 0, 0, null); //draw background
+	}
+	
+	public void setPlayerNum(int i){
+		totalPlayerNum =i;
+	}
+	
+	private class ButtonListener implements ActionListener{
+		public void actionPerformed(ActionEvent event){
+			if(event.getSource()==nextBut){
+				if(name!="" && currentFaction != 0){
+					nameList.add(name);
+					if(currentFaction==1){
+						factionList.add("rohan");
+						rohan.setSelected(false);
+						rohan.setEnabled(false);
+					}
+					else if(currentFaction == 2){
+						factionList.add("gondor");
+						gondor.setSelected(false);
+						gondor.setEnabled(false);
+					}
+					else if(currentFaction ==3){
+						factionList.add("elves");
+						elves.setSelected(false);
+						elves.setEnabled(false);
+					}
+					else if(currentFaction==4){
+						factionList.add("dwarfs");
+						dwarfs.setSelected(false);
+						dwarfs.setEnabled(false);
+					}
+					else if(currentFaction==5){
+						factionList.add("mordor");
+						mordor.setSelected(false);
+						mordor.setEnabled(false);
+					}
+					else if(currentFaction==6){
+						factionList.add("harad");
+						harad.setSelected(false);
+						harad.setEnabled(false);
+					}
+					else if(currentFaction==7){
+						factionList.add("isengard");
+						isengard.setSelected(false);
+						isengard.setEnabled(false);
+					}
+					askName.setText("");
+					if(totalPlayerNum>1){
+						totalPlayerNum--;
+					}
+					else if(totalPlayerNum==1){
+						cardLayout.show(mainPanel, "gameMainPanel");
+					}
+				}
+				
+			}
+		}
+	}
+	
+	
+	private class BackToMenuListener implements KeyListener{
+		public void keyPressed(KeyEvent event){
+			if(event.getKeyCode() == KeyEvent.VK_ESCAPE){
+				cardLayout.show(mainPanel, "menu");
+			}
+		}
+		public void keyTyped(KeyEvent event){}
+		public void keyReleased(KeyEvent event){}
+	}
+	
+	private class NameListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			name = askName.getText();
+		}
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	private class FactionListener implements ActionListener{
+		public void actionPerformed(ActionEvent event){
+			if(event.getSource() == rohan){
+				currentFaction=1;
+			}
+			else if(event.getSource() == gondor){
+				currentFaction=2;
+			}
+			else if(event.getSource() == elves){
+				currentFaction=3;
+			}
+			else if(event.getSource() == dwarfs){
+				currentFaction=4;
+			}
+			else if(event.getSource() == mordor){
+				currentFaction=5;
+			}
+			else if(event.getSource() == harad){
+				currentFaction=6;
+			}
+			else if(event.getSource() == isengard){
+				currentFaction=7;
+			}
+		}
+	}
+	
+
+}
---
+++ MenuPanel.java
@@ -0,0 +1,110 @@
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+/*import java.io.File;
+import java.io.IOException;
+import java.util.*;*/
+
+
+public class MenuPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Image background;
+	private ImageIcon[] buttonImages;
+	private JButton button[];
+	private JPanel mainPanel;
+	private CardLayout cardLayout;
+	
+	public MenuPanel(JPanel mP, CardLayout cl) {
+		
+		cardLayout=cl;
+		mainPanel=mP;
+		
+		button = new JButton[4]; //creating buttons
+
+		buttonImages = new ImageIcon[4]; //button images created
+		
+		//button images loaded
+		buttonImages[0]=new ImageIcon("images/play multiplayer mode.png");
+		buttonImages[1]=new ImageIcon("images/options.png");
+		buttonImages[2]=new ImageIcon("images/help.png");
+		buttonImages[3]=new ImageIcon("images/quit game.png");
+		
+		//buttons created
+		button[0] = new JButton("");
+		button[1] = new JButton("");
+		button[2] = new JButton("");
+		button[3] = new JButton("");
+		
+		//buttons added to the menu panel
+		add(button[0]);		
+		add(button[1]);
+		add(button[2]);
+		add(button[3]);
+		
+		//the sizes and locations of the buttons set
+		button[0].setBounds(185, 253, 252, 63);		
+		button[1].setBounds(185, 327, 252, 63);
+		button[2].setBounds(185, 401, 252, 63);
+		button[3].setBounds(185, 475, 252, 63);
+		
+		//button listener created
+		ButtonListener listener = new ButtonListener();
+		
+		//makes frames of the buttons opac and add listener to button and set its icon
+		for(int i=0; i<4; i++){
+			button[i].setIcon(buttonImages[i]);
+			button[i].setFocusPainted(false); 
+			button[i].setOpaque(false);
+			button[i].setContentAreaFilled(false);
+			button[i].setBorderPainted(false);	
+			button[i].setFocusable(false);
+			button[i].addActionListener(listener);
+		}
+		
+		
+		//set the bounds of the panel
+		setBounds(0, 0, 650, 650);
+		
+		//to change the places of buttons on the panel
+		setLayout(null);
+		
+		//background color of the panel
+		setBackground(Color.black);
+		setVisible(true);
+		
+	}
+	
+	//paint component to set background
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);		
+		background = new ImageIcon("images/background.jpg").getImage(); //get background icon as image
+		
+		g.drawImage( background, 0, 0, null); //draw background
+	}
+	
+	public static void main(String[] args){}
+	
+	
+	private class ButtonListener implements ActionListener{
+		public void actionPerformed(ActionEvent event){
+			if(event.getSource()==button[0]){
+				cardLayout.show(mainPanel, "playerNumPanel");
+			}
+			else if(event.getSource()==button[1]){
+				cardLayout.show(mainPanel, "options");
+			}
+			else if(event.getSource()==button[2]){
+				cardLayout.show(mainPanel, "help");
+			}
+			else if(event.getSource()==button[3]){
+				System.exit(0);
+			}
+		}
+	}
+	
+}
